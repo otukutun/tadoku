b3a3db9ab897e2d3fd5304d706e62d7ab8c5fd23
otukutun <orehaorz@gmail.com>
2013-08-02 18:04:25 +0900

    Initial commit

---
+++ myapp/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'otukutun',
+		'password' => 'otukutun',
+		'database' => 'tadoku',
+		'encoding' => 'utf8'
+	);
+}
