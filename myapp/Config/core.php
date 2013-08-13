5d13b88ab10473a310c6ef9b76370f4010cfd45c
otukutun <orehaorz@gmail.com>
2013-08-14 00:09:15 +0900

    SplFileInfo::openFileの対処

--- myapp/Config/core.php
+++ myapp/Config/core.php
@@ -338,7 +338,8 @@ Cache::config('_cake_core_', array(
 	'prefix' => $prefix . 'cake_core_',
 	'path' => CACHE . 'persistent' . DS,
 	'serialize' => ($engine === 'File'),
-	'duration' => $duration
+    'duration' => $duration,
+    'mask' => 0666
 ));
 
 /**
@@ -350,7 +351,8 @@ Cache::config('_cake_model_', array(
 	'prefix' => $prefix . 'cake_model_',
 	'path' => CACHE . 'models' . DS,
 	'serialize' => ($engine === 'File'),
-	'duration' => $duration
+	'duration' => $duration,
+    'mask' => 0666
 ));
 
 Cache::config('default', array(
