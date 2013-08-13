3bae9d99a99a3d80e762542bc918af2c43cfcaca
otukutun <orehaorz@gmail.com>
2013-08-13 15:28:31 +0900

    tmp以下のディレクトリをつくるやつをシェルスクリプトを作成

---
+++ mk_dir_tmp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'tmp以下のディレクトリを作成します'
+mkdir tmp
+mkdir tmp/cache
+mkdir tmp/cache/models
+mkdir tmp/cache/persistent
+mkdir tmp/cache/views
+mkdir tmp/logs
+mkdir tmp/sessions
+mkdir tmp/tests
